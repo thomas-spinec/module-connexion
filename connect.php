991cdfb52e8c056e97ce0209604ec0f77c4cd702
thomas-spinec <thomas.spinec@laplateforme.io>
2022-12-02 10:21:24 +0100

    ajout de la variable de connexion pour plesk

--- connect.php
+++ connect.php
@@ -1,5 +1,9 @@
 <?php
-// Connexion à la base de données
+// Connexion à la base de données en local
+
 $connect = new mysqli('localhost', 'root', '', 'moduleconnexion');
 
+// Connexion à la base de données en ligne
+
+// $connect = new mysqli('localhost', 'Thomas', 'mdpdatabase', 'thomas-spinec_moduleconnexion')
 ?>
